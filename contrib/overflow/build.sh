4b11ec6bc39fcab4235f4200a239bde14fac5a1b
SekoiaTree <sekoia@sekoia.dev>
2025-05-15 22:24:33 +0200

    fix: simplify build.sh

--- contrib/overflow/build.sh
+++ contrib/overflow/build.sh
@@ -1,6 +1,2 @@
-autoupdate
-autoreconf -f -i
-./configure
-make -j$(nproc) clean
-make -j$(nproc) libpng15.la
-g++ -std=c99 -fpermissive ../read_to_gray.c -I. -o read_to_gray .libs/libpng15.a -lz
+make -j$(nproc) -f scripts/makefile.std
+g++ -std=c99 -fpermissive contrib/overflow/read_to_gray.c -I. -o read_to_gray libpng.a -lz
